85815b24a78b3f978c3ccf8d4f91ab8a7f466642
Abel <abeljohnjoseph@gmail.com>
2020-03-25 18:23:15 -0400

    Complete classification

--- sequential_classifier.py
+++ sequential_classifier.py
@@ -8,8 +8,6 @@ class sequential_classifier:
 	def __init__(self, A, B):
 		self.A = A
 		self.B = B
-		self.prototype_A = A[random.randint(0, len(A) - 1)]
-		self.prototype_B = B[random.randint(0, len(B) - 1)]
 
 	# Adapted from lab 1
 	@staticmethod
@@ -17,37 +15,65 @@ class sequential_classifier:
 		return sqrt((px0 - px1) ** 2 + (py0 - py1) ** 2)
 
 	# Adapted from lab 1
-	def get_med(self, a, b):
-		dist_a = sequential_classifier.get_euclidean_dist(self.prototype_A[0], self.prototype_A[1], a, b)
-		dist_b = sequential_classifier.get_euclidean_dist(self.prototype_B[0], self.prototype_B[1], a, b)
+	def get_med(self, a, b, prototype_A, prototype_B):
+		dist_a = sequential_classifier.get_euclidean_dist(prototype_A[0], prototype_A[1], a, b)
+		dist_b = sequential_classifier.get_euclidean_dist(prototype_B[0], prototype_B[1], a, b)
 
 		return 1 if dist_a < dist_b else 2
 
-	def perform_classification(self, j=1):
-		misclassified = True
-		mis_A = []  # Misclassified points
-		mis_B = []
+	def perform_classification(self, J):
+		j = 1
+		discriminants = []
+		true_n_ab = []
+		true_n_ba = []
 
-		while misclassified:
-			n_ba = 0  # Error count
-			n_ab = 0
+		A = self.A
+		B = self.B
+		prototype_A = 0
+		prototype_B = 0
+		n_ba = 0  # Error count
+		n_ab = 0
 
-			# Classify all points for A
-			for i, pt in enumerate(self.A):
-				res = self.get_med(pt[0], pt[1])
+		while True:
+			misclassified = True
 
-				if res == 2:  # Misclassified
-					n_ab += 1
-					mis_A.append(pt)
+			while misclassified:
+				mis_A = []  # Misclassified points
+				mis_B = []
 
-			# Classify all points for B
-			for i, pt in enumerate(self.B):
-				res = self.get_med(pt[0], pt[1])
+				if len(A): prototype_A = A[random.randint(0, len(A) - 1)]
+				if len(B): prototype_B = B[random.randint(0, len(B) - 1)]
 
-				if res == 1:  # Misclassified
-					n_ba += 1
-					mis_B.append(pt)
+				# Classify all points for A
+				for i, pt in enumerate(A):
+					res = self.get_med(pt[0], pt[1], prototype_A, prototype_B)
 
-			if not n_ab or not n_ba:  # No misclassified pts
-				misclassified = False
+					if res == 2:  # Misclassified
+						n_ab += 1
+						mis_A.append(pt)
+
+				# Classify all points for B
+				for i, pt in enumerate(B):
+					res = self.get_med(pt[0], pt[1], prototype_A, prototype_B)
+
+					if res == 1:  # Misclassified
+						n_ba += 1
+						mis_B.append(pt)
+
+				if not n_ab or not n_ba:  # No misclassified pts
+					# Remove points from b that were classified as B
+					if not n_ab:
+						B = mis_B
+					if not n_ba:
+						A = mis_A
+					misclassified = False
+
+			discriminants.extend([prototype_A, prototype_B])
+			true_n_ab.append(n_ab)
+			true_n_ba.append(n_ba)
+
+			if (J and j > J) or (not A and not B):
+				break
+
+			j += 1
 
