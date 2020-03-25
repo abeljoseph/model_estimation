6e249a766c0ee93f34f5c2c5a2696d16a5c8b21f
Abel <abeljohnjoseph@gmail.com>
2020-03-24 21:17:51 -0400

    Fix list creation, add linspace

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-def main(): 
+import numpy as np 
+
+def main():
 	# Section 3
 	points_a = []
 	points_b = []
 
 	with open("data_files/csv/lab2_3_a.csv", 'r') as datafile_a, open("data_files/csv/lab2_3_b.csv", 'r') as datafile_b:
 		for line in datafile_a:
-			points_a.append(list(line))
+			points_a.append([int(x) for x in line.split(',')])
 
 		for line in datafile_b:
-			points_b.append(list(line))
+			points_b.append([int(x) for x in line.split(',')])
+
+	points_a = np.array(points_a)
+	points_b = np.array(points_b)
+
+
+	num_steps = 100
+
+	# Create Meshgrid for MED Classification
+	x_grid = np.linspace(min(*points_a[:, 0], *points_b[:, 0]), max(*points_a[:, 0], *points_b[:, 0]),
+						 num_steps)
+	y_grid = np.linspace(min(*points_a[:, 1], *points_b[:, 1]), max(*points_a[:, 1], *points_b[:, 1]),
+						 num_steps)
+
 
 if __name__ == "__main__":
 	main()
